52d061a522c3e7ed812cb6d37ccc8d03040f0670
Leticia Santos <102737252+leticiaholsback@users.noreply.github.com>
2022-11-07 18:48:21 -0300

    Add files via upload

---
+++ quickSort.c
@@ -0,0 +1,16 @@
+﻿"aprende menina.exe" (Win32): Carregado "C:\Users\dvalv\source\repos\aprende menina\x64\Debug\aprende menina.exe". Símbolos carregados.
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\ntdll.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\kernel32.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\KernelBase.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\vcruntime140d.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\ucrtbased.dll". 
+O thread 0x11ac foi fechado com o código 0 (0x0).
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\sechost.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\rpcrt4.dll". 
+O thread 0x30c0 foi fechado com o código 0 (0x0).
+O thread 0x1094 foi fechado com o código 0 (0x0).
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\kernel.appcore.dll". 
+"aprende menina.exe" (Win32): Carregado "C:\Windows\System32\msvcrt.dll". 
+O thread 0x2168 foi fechado com o código 0 (0x0).
+O thread 0x2948 foi fechado com o código 0 (0x0).
+O programa "[14744] aprende menina.exe" foi fechado com o código 0 (0x0).
