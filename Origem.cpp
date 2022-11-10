e0009326e383951ff1c14ca6c6e5aae4fa5d24b6
Leticia Santos <102737252+leticiaholsback@users.noreply.github.com>
2022-11-10 09:06:31 -0300

    Grafos em lista

--- Origem.cpp
+++ Origem.cpp
@@ -1,199 +1,80 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int menu();
-void InserirInicio(int num);
-void InserirFim(int num);
-void InserirMeio(int num, int posicao);
-int Remover(int num);
-void Listar();
+struct ListaDeVizinhos
+{
+	int vertice;
+	ListaDeVizinhos* prox;
+};
 
-struct ElementoDaLista_Simples {
-	int dado;
-	ElementoDaLista_Simples *prox; //* indicaa inicializacao de uma variavel tipo ponteiro
-} *Head; // variavel global
+struct Grafo
+{
+	int TotalVertices;
+	struct ListaDeVizinhos** ListaAdj; //Será um vetor do tamanho do numero de vértices
+};
+
+struct Grafo* CriarGrafo(int vertices);
+void AdicionarAresta(struct Grafo* Grafo, int origem, int destino);
+void MostrarGrafo(struct Grafo* Grafo);
 
 int main()
 {
-	int op, num, pos, c;
-	while (1) {
-		op = menu();
-		switch (op) {
-		case 1: // inserir no inicio da lista
-			printf("Digite o numero desejado: ");
-			scanf_s("%d", &num);
-			InserirInicio(num);
-			break;
-		case 2: // inserir no fuim da lista
-			printf("Digite o numero desejado: ");
-			scanf_s("%d", &num);
-			InserirFim(num);
-			break;
-		case 3: // inserir no meio da lista
-			printf("Digite o numero desejado: ");
-			scanf_s("%d", &num); 
-			while ((c = getchar()) != '\n' && c != EOF) {} // buffer de limpeza de teclado, usar apos cada scanf
-			printf("Digite a posicao desejada: ");
-			scanf_s("%d", &pos);
-			while ((c = getchar()) != '\n' && c != EOF) {} // buffer de limpeza de teclado, usar apos cada scanf
-			InserirMeio(num, pos);
-			break;
-		case 4: // remover da lista
-			int res;
-			printf("Digite o numero a ser removido: ");
-			scanf_s("%d", &num);
-			while ((c = getchar()) != '\n' && c != EOF) {} // sempre limpe o buffer do teclado.
-			res = Remover(num);
-			if (res == 1)
-				printf("Numero removido.");
-			else
-				printf("Numero nao encontrado.");
-			break;
-		case 5: // mostrar toda a lista
-			Listar();
-			break;
-		case 6:
-			return 0;
-		default:
-			printf("Invalido/n");
-		}
-	}
-	return 0;
-}
+	Grafo* GrafoTeste = CriarGrafo(6);
+	AdicionarAresta(GrafoTeste, 0, 1);
+	AdicionarAresta(GrafoTeste, 0, 2);
+	AdicionarAresta(GrafoTeste, 1, 3);
+	AdicionarAresta(GrafoTeste, 2, 3);
+	AdicionarAresta(GrafoTeste, 3, 4);
+	AdicionarAresta(GrafoTeste, 3, 5);
 
-int menu() {
-	int op, c;
-	system("Cls");
-
-	printf("VAMOS ENTENDER LISTAS ENCADEADAS? ESCOLHA A OPCAO: \n" "<3 LETICIA, NAO SE ESQUECA, VOCE COSNEGUE! <3\n");
-	printf("1. Inserir no INICIO da lista encadeada simples\n");
-	printf("2. Inserir no FIM da lista encadeada simples\n");
-	printf("3. Inserir no MEIO da lista encadeada simples\n");
-	printf("4. REMOVER da lista encadeada simples\n");
-	printf("5. MOSTRAR lista encadeada simples\n");
-	printf("6. SAIR da lista\n");
-	printf("Digite sua escolha:\n");
-
-	scanf_s("%d", &op);
-	while ((c = getchar()) != '\n' && c != EOF) {}
-
-	system("C1s");
-	return op;
-}
-
-void InserirInicio(int num) // lista simples nao cuircular
-{
-	ElementoDaLista_Simples *NovoElemento;
-	//alocar o elemento na memria
-	NovoElemento = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-	NovoElemento->dado = num; // num vai para NovoElemento dado
-
-	if (Head == NULL)
-	{
-		Head = NovoElemento;
-		Head->prox = NULL;
-	}
-	else
-	{
-		NovoElemento->prox = Head;
-		Head = NovoElemento;
-	}
-}
-void InserirFim(int num)  // lista FIM
-{
-	ElementoDaLista_Simples *NovoElemento;
-	NovoElemento = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-	ElementoDaLista_Simples* ElementoVarredura;
-	ElementoVarredura = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-
-	NovoElemento->dado = num;
-	if (Head == NULL)
-	{
-		Head = NovoElemento;
-		Head->prox = NULL;
-
-	}
-	else
-	{
-		ElementoVarredura = Head;
-		while (ElementoVarredura->prox != NULL) // != (( diferente))
-			ElementoVarredura = ElementoVarredura->prox; //transforma o elemento em proximo elemento da lista encadeada
-
-		ElementoVarredura->prox = NovoElemento;
-		NovoElemento->prox = NULL;
-	}
-}
-void InserirMeio(int num, int posicao) // lista MEIO
-{
-	ElementoDaLista_Simples *NovoElemento;
-	NovoElemento = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-	ElementoDaLista_Simples *ElementoVarredura;
-	ElementoVarredura = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-	ElementoDaLista_Simples *ElementoAuxiliar;
-	ElementoAuxiliar = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples));
-	
-	 NovoElemento->dado = num;
-	if (posicao == 0) 
-	{
-		Head = NovoElemento;
-		Head->prox = NULL;
-	}
-	else
-	{
-		ElementoVarredura = Head;
-		for (int i = 0; i < posicao - 1; i++)
-			ElementoVarredura = ElementoVarredura->prox;
-
-		ElementoAuxiliar = ElementoVarredura->prox;
-		ElementoVarredura->prox = NovoElemento;
-		NovoElemento->prox = ElementoAuxiliar;
-	}
-}
-int Remover(int num) //Lista 4. REMOVER
-{
-	ElementoDaLista_Simples* ElementoVarredura;
-	ElementoVarredura = (struct ElementoDaLista_Simples*)malloc(sizeof(struct ElementoDaLista_Simples));
-	ElementoDaLista_Simples* Anterior;
-	Anterior = (struct ElementoDaLista_Simples*)malloc(sizeof(struct ElementoDaLista_Simples));
-
-	ElementoVarredura = Head;
-	while (ElementoVarredura != NULL) {
-		if (ElementoVarredura->dado == num) {
-			if (ElementoVarredura == Head) {
-				Head = ElementoVarredura->prox;
-				free(ElementoVarredura);
-				return 1;
-			}
-			else {
-				Anterior->prox = ElementoVarredura->prox;
-				free(ElementoVarredura);
-				return 1;
-			}
-		}
-		else {
-			Anterior = ElementoVarredura;
-			ElementoVarredura = ElementoVarredura->prox;
-		}
-	}
-	return 0;
-}
-
-void Listar()  // Lista 5. Mostrar
-{
-	ElementoDaLista_Simples* ElementoVarredura;
-	ElementoVarredura = (struct ElementoDaLista_Simples*)malloc(sizeof(struct ElementoDaLista_Simples));
-
-	ElementoVarredura = Head;
-	if (ElementoVarredura == NULL) {
-		return;
-	}
-	while (ElementoVarredura != NULL) {
-		printf("%d", ElementoVarredura->dado);
-		ElementoVarredura = ElementoVarredura->prox;
-	}
-	printf("\n");
-
+	MostrarGrafo(GrafoTeste);
 	system("pause");
-	return;
+	return 0;
 }
 
+struct Grafo* CriarGrafo(int TotalVertices)
+{
+	Grafo* Grafo = (struct Grafo*)malloc(sizeof(struct Grafo));
+	Grafo->TotalVertices = TotalVertices;
+	Grafo->ListaAdj = (struct ListaDeVizinhos**)malloc(TotalVertices * sizeof(struct ListaDeVizinhos*));
+
+	for (int i = 0; i < TotalVertices; i++)
+		Grafo->ListaAdj[i] = NULL;
+
+	return Grafo;
+}
+
+void AdicionarAresta(struct Grafo* Grafo, int origem, int destino)
+{ //INSERÇÃO NO INICIO DA LISTA 
+	// Adiciona uma aresta da origem para o  destino
+	struct ListaDeVizinhos* NovoElemento = (struct ListaDeVizinhos*)malloc(sizeof(struct ListaDeVizinhos));
+	NovoElemento->vertice = destino;
+	NovoElemento->prox = NULL;
+
+	NovoElemento->prox = Grafo->ListaAdj[origem];
+	Grafo->ListaAdj[origem] = NovoElemento;
+
+	// Adiciona uma aresta do destino para a origem
+	NovoElemento = (struct ListaDeVizinhos*)malloc(sizeof(struct ListaDeVizinhos));
+	NovoElemento->vertice = origem;
+	NovoElemento->prox = NULL;
+
+	NovoElemento->prox = Grafo->ListaAdj[destino];
+	Grafo->ListaAdj[destino] = NovoElemento;
+}
+
+void MostrarGrafo(struct Grafo* Grafo)
+{
+	int v;
+	for (v = 0; v < Grafo->TotalVertices; v++)
+	{
+		struct ListaDeVizinhos* ElementoVarredura = Grafo->ListaAdj[v];
+		printf("\n Lista de adjacencias do vertice %d:\n ", v);
+		while (ElementoVarredura != NULL)
+		{
+			printf("%d -> ", ElementoVarredura->vertice);
+			ElementoVarredura = ElementoVarredura->prox;
+		}
+		printf("\n");
+	}
+}
